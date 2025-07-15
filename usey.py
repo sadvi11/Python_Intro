a8bc2d828466f761c4d1e0cc1d71a3d667c3c655
sadvi11 <sadhvikhajuria@gmail.com>
2025-07-15 21:51:12 +0530

    add python files

---
+++ usey.py
@@ -0,0 +1,12 @@
+print("i want to capture your details")
+print("enter your surname")
+surname=input()
+print("enter your first name")
+firstname=input()
+print("enter your email")
+email=input()
+print("enter your phone")
+phone=input()
+print("my details : " ,surname ,firstname ,email ,phone)
+
+
